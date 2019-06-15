00686e897c28ff56bcb287cd1c78f686cb29a999
GinoQuispe21 <gino_21quispe@outlook.com>
2019-06-15 01:18:07 -0500

    Actualizacion de interfaz dinamica con el usuario.

--- FDS1/Source.cpp
+++ FDS1/Source.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace System;
 
-void mostrarm2(Controladora*& control) {
+void mostrarFiltro(Controladora*& control) {
 	int col, opcion;
 	cout << "Ingrese columna: " << endl;
 	cin >> col;
@@ -41,22 +41,121 @@ void mostrarm2(Controladora*& control) {
 	} while (opcion == 0);
 	
 }
+void mostrarm1(Controladora*& control) {
+	int op;
+	do {
+		cout << "Seleccione tipo de separador (1: ' ' ; 2: ',')"; cin>>op;
+	} while (!(op == 1 || op == 2));
+	switch (op){
+	case 1:control->leerDatosString(); break;
+	case 2:control->leerDatosString(); break;
+	default:
+		break;
+	}
+}
+void mostrarm2(Controladora*& control) {
+	int opcion;
+	cout << "Seleccione Dataframe "; cin >> opcion;
+}
+void mostrarm3(Controladora*& control) {
+	int opc;
+	cout << "*************************************" << endl;
+	cout << "*          CREAR DATAFRAME          *" << endl;
+	cout << "*                                   *" << endl;
+	cout << "*      1> Creacion desde cero       *" << endl;
+	cout << "*      2> Desde otro Dataframe      *" << endl;
+	cout << "*                                   *" << endl;
+	cout << "*************************************" << endl;
+	cout << endl;
+	do {
+		cout << "Ingrese la opcion : "; cin >> opc;
+	} while (!(opc == 1 || opc == 2));
+	switch (opc)
+	{
+	case 1:
+		cout << endl;
+		cout << " Columna " << endl;
+		cout << " Fila    " << endl;
+ 		break;
+	case 2:
+		int op;
+		cout << endl;
+		cout << " 1 > Seleccionar Dataframe " << endl;
+		cout << " 2 > Operaciones con Dataframe " << endl;
+		do{
+			cout << "Ingrese la opcion : "; cin >> op;
+		} while (!(op == 1 || op == 2));
+		if (op == 1) {
+			cout << "-----------------" << endl;
+			break;
+		}
+		else if (op == 2) {
+			cout << endl;
+			cout << " Seleccionar columna " << endl;
+			cout << " Filtrar " << endl;
+			mostrarFiltro(control);
+		}
+	default:
+		break;
+	}
+
+}
+
+//void showM() {
+//	cout << endl << endl << endl << endl << endl;
+//	cout << "                         ***************************************************************************" << endl;
+//	cout << "                         *                                                                         *" << endl;
+//	cout << "                         *                                 MENU                                    *" << endl;
+//	cout << "                         *                                                                         *" << endl;
+//	cout << "                         *        1.- Importar                                                     *" << endl;
+//	cout << "                         *        2.- mostrar1                                                     *" << endl;
+//	cout << "                         *        3.- Exportar                                                     *" << endl;
+//	cout << "                         *        4.- Filtrar                                                      *" << endl;
+//	cout << "                         *        5.- mostrar2                                                     *" << endl;
+//	cout << "                         *        6.- Ordenar columna                                              *" << endl;
+//	cout << "                         *        7.- Salir                                                        *" << endl;
+//	cout << "                         *                                                                         *" << endl;
+//	cout << "                         ***************************************************************************" << endl;
+//}
+
 void showM() {
 	cout << endl << endl << endl << endl << endl;
 	cout << "                         ***************************************************************************" << endl;
 	cout << "                         *                                                                         *" << endl;
-	cout << "                         *                                 MENU                                    *" << endl;
+	cout << "                         *                                  MENU                                    *" << endl;
 	cout << "                         *                                                                         *" << endl;
-	cout << "                         *        1.- Importar                                                     *" << endl;
-	cout << "                         *        2.- mostrar1                                                     *" << endl;
-	cout << "                         *        3.- Exportar                                                     *" << endl;
-	cout << "                         *        4.- Filtrar                                                      *" << endl;
-	cout << "                         *        5.- mostrar2                                                     *" << endl;
-	cout << "                         *        6.- Ordenar columna                                              *" << endl;
-	cout << "                         *        7.- Salir                                                        *" << endl;
+	cout << "                         *        1.- Cargar Dataframe                                             *" << endl;
+	cout << "                         *        2.- Guardar Dataframe                                            *" << endl;
+	cout << "                         *        3.- Listar Dataframe                                             *" << endl;
+	cout << "                         *        4.- Crear Dataframe                                              *" << endl;
+	cout << "                         *        5.- Indexar Dataframe                                            *" << endl;
+	cout << "                         *        6.- Salir                                                        *" << endl;
 	cout << "                         *                                                                         *" << endl;
 	cout << "                         ***************************************************************************" << endl;
 }
+//void main() {
+//	Controladora* control = new Controladora();
+//	int opcion = 0;
+//	showM();
+//	do {
+//		cin >> opcion;
+//		switch (opcion) {
+//		case 1:control->leerDatosString(); opcion = 0; break;
+//		case 2:; control->mostrar(1); opcion = 0; break;
+//		case 3:; opcion = 0; break;
+//		case 4:  mostrarFiltro(control); opcion = 0; break;
+//		case 5:; control->mostrar(2); opcion = 0; break;
+//		case 6:; opcion = 0; break;
+//		case 7:; exit(1); break;
+//		default: opcion = 0; break;
+//		}
+//	} while (opcion == 0);
+//
+//	control->leerDatos();
+//
+//	_getch();
+//}
+
 void main() {
 	Controladora* control = new Controladora();
 	int opcion = 0;
@@ -64,13 +163,12 @@ void main() {
 	do {
 		cin >> opcion;
 		switch (opcion) {
-		case 1:control->leerDatosString(); opcion = 0; break;
-		case 2:; control->mostrar(1); opcion = 0; break;
-		case 3:; opcion = 0; break;
-		case 4:  mostrarm2(control); opcion = 0; break;
-		case 5:; control->mostrar(2); opcion = 0; break;
-		case 6:; opcion = 0; break;
-		case 7:; exit(1); break;
+		case 1: mostrarm1(control); opcion = 0; break;
+		case 2: mostrarm2(control); opcion = 0; break;
+		case 3: control->mostrar(1); opcion = 0; break;
+		case 4: mostrarm3(control); control->mostrar(2); opcion = 0; break;
+		case 5: mostrarm2; break;
+		case 6: exit(1); break;
 		default: opcion = 0; break;
 		}
 	} while (opcion == 0);
